3ae6e4fd75e4ec070cb509256e637577da8e6ffa
EseVic <esevic111@gmail.com>
2023-04-26 08:12:41 -0700

    hsh handle

--- hsh_handlers1.c
+++ hsh_handlers1.c
@@ -9,13 +9,13 @@
 int is_interactive(sh_args *content)
 {
 	/* Check if stdin is connected to a terminal */
-    int is_stdin_tty = isatty(STDIN_FILENO);
+	int is_stdin_tty = isatty(STDIN_FILENO);
 
 	/* Check if the file descriptor for the shell's input is less than */
 	/* or equal to 2 (stdin, stdout, or stderr) */
-    int is_readfd_valid = content->readfd <= 2;
+	int is_readfd_valid = content->readfd <= 2;
 
-    return (is_stdin_tty && is_readfd_valid);
+	return (is_stdin_tty && is_readfd_valid);
 }
 
 
@@ -23,7 +23,7 @@ int is_interactive(sh_args *content)
  * is_delimiter - checks if a given character is a delimeter character
  * @car: character being check
  * @delimiter: delimeter string
- * 
+ *
  * Return: 1 (true), 0 (false)
  */
 int is_delimiter(char car, char *delim)
@@ -31,15 +31,15 @@ int is_delimiter(char car, char *delim)
 	size_t idx;
 
 	/* Iterate over the delimiter string until a null terminator is reached */
-    for (idx = 0; delim[idx]; ++idx)
+	for (idx = 0; delim[idx]; ++idx)
 	{
 		/* If the current delimiter matches the given character, return true */
-        if (car == delim[idx])
+	if (car == delim[idx])
 		{
-            return 1;
-        }
-    }
+		return (1);
+	}
+	}
 
 	/* If no delimiter matches the given character, return false */
-    return 0;
+	return (0);
 }
