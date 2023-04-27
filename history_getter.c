c258431a46b97f4314956d70b9ea7ef80d73f389
EseVic <esevic111@gmail.com>
2023-04-27 15:35:02 -0700

    history getter

--- history_getter.c
+++ history_getter.c
@@ -1,106 +1,5 @@
 #include "shell.h"
 
-/**
- * load_history - reads command history from
- * a file and add it to a history buffer
- * @content: struct parameter
- *
- * Return: histcount (success), 0 (error)
- */
-int load_history(sh_args *content)
-{
-	char *history_file_path;
-	char *history_buffer;
-	int del_idx;
-	int idx = 0;
-	int history_fil_des;
-	int line_count = 0;
-	int last_newline_index = 0;
-	struct stat history_stat;
-	ssize_t bytes_read;
-
-	/* gets the path to the history file */
-	history_file_path = get_hist_file_path(content);
-	if (!history_file_path)
-	{
-		return (0);
-	}
-
-	/* opens the history file */
-	history_fil_des = open(history_file_path, O_RDONLY);
-	free(history_file_path);
-	if (history_fil_des == -1)
-	{
-		return (0);
-	}
-
-	/* gets information about the history file */
-	if (fstat(history_fil_des, &history_stat) == -1)
-	{
-		close(history_fil_des);
-		return (0);
-	}
-
-	/* checks if history file is empty or too small */
-	if (history_stat.st_size < 2)
-	{
-		close(history_fil_des);
-		return (0);
-	}
-
-	history_buffer = (char*) malloc(history_stat.st_size + 1);
-	if (!history_buffer)
-	{
-		close(history_fil_des);
-		 return (0);
-	}
-
-	/* reads history file into buffer */
-	bytes_read = read(history_fil_des, history_buffer, history_stat.st_size);
-	history_buffer[history_stat.st_size] = '\0';
-	close(history_fil_des);
-
-	/* checks if history file was read successfully */
-	if (bytes_read <= 0)
-	{
-		free(history_buffer);
-		return (0);
-	}
-
-	/* iterates through history buffer and add entries to history linked list */
-	while (idx < history_stat.st_size)
-	{
-		if (history_buffer[idx] == '\n')
-		{
-			history_buffer[idx] = '\0';
-			add_to_history(content, history_buffer + last_newline_index, line_count);
-			last_newline_index = idx + 1;
-			line_count++;
-		}
-		idx++;
-	}
-
-	/* If there is still text in the buffer, add it as a new entry */
-	if (last_newline_index != history_stat.st_size)
-	{
-		add_to_history(content, history_buffer + last_newline_index, line_count);
-		line_count++
-	}
-
-	free(history_buffer);
-
-	/* If the history list is too long, delete the oldest entry */
-	for (del_idx = content->histcount; del_idx >= HIST_MAX; del_idx--)
-	{
-		delete_node_index(&(content->history), 0);
-	}
-
-	/* Update the history entry numbers */
-	update_hist_node_numbrs(content);
-
-	return (content->histcount);
-}
-
 
 /**
  * write_shel_histry - creates a file to store
@@ -112,8 +11,8 @@ int load_history(sh_args *content)
 int write_shel_histry(sh_args *content)
 {
 	/* gets the path to the history file */
-    char *history_file = get_hist_file_path(content);
-    int file_des;
+	char *history_file = get_hist_file_path(content);
+	int file_des;
 	l_list *node;
 
 	if (!history_file)
@@ -167,7 +66,10 @@ int add_to_history(sh_args *content, char *buffer, int linecount)
 	return (-1);
 
 	/* traverses the linked list until we reach the end */
-	for ( ; *tail_ptr != NULL; tail_ptr = &((*tail_ptr)->link));
+	for ( ; *tail_ptr != NULL; tail_ptr = &((*tail_ptr)->link))
+	{
+
+	}
 
 	/* creates a new node and add it to the end of the list */
 	new_end_node(tail_ptr, buffer, linecount);
