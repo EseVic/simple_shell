581cefee4eb53daba18c0facf665abebe8a362c5
EseVic <esevic111@gmail.com>
2023-04-28 15:17:25 -0700

    exit checker

--- exit_checker.c
+++ exit_checker.c
@@ -25,16 +25,16 @@
  */
 int change_directory(sh_args *content)
 {
-	char *wrkin_dir, *dir, buff[1024];
 	int cd_success;
+	char buff[1024], *wrkin_dir, *dir;
 
 	wrkin_dir = getcwd(buff, 1024);
-	if (!wrkin_dir)
+	if (wrkin_dir == NULL)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!content->argv[1])
 	{
 		dir = getenv_clone(content, "HOME=");
-		if (!dir)
+		if (dir == NULL)
 			cd_success = chdir((dir = getenv_clone(content, "PWD=")) ? dir : "/");
 		else
 			cd_success = chdir(dir);
@@ -75,12 +75,17 @@ int change_directory(sh_args *content)
  */
 int help_command(sh_args *content)
 {
-	char **arg_v;
+	char **arg_v = content->argv;
 
-	arg_v = content->argv;
-	_puts("help call works. Function not yet implemented \n");
+	/* If no argument is provided, show general help message */
+	_puts("Welcome to the Shell Help Menu:\n   help: Show this help menu\n");
+	_puts("   cd: Change directory\n   exit: Exit the shell\n");
+
 	if (0)
-		_puts(*arg_v); /* temp att_unused workaround */
+	{
+		_puts(*arg_v);
+	}
+
 	return (0);
 }
 
@@ -95,13 +100,16 @@ int shell_exit(sh_args *content)
 {
 	int exitcheck;
 
-	if (content->argv[1])  /* If there is an exit arguement */
+	if (content->argv[1])
 	{
+		/* converts exit argument to an integer */
 		exitcheck = exit_str_to_int(content->argv[1]);
+
+		/* If the exit arg is not a valid integer, print an err mesg */
 		if (exitcheck == -1)
 		{
 			content->status = 2;
-			print_err_mesg(content, "Illegal number: ");
+			print_err_mesg(content, "Error number: ");
 			write_string_with_buffer(content->argv[1]);
 			write_with_buffer('\n');
 			return (1);
@@ -130,15 +138,19 @@ int shell_exit(sh_args *content)
  */
 int exit_str_to_int(char *str)
 {
-	int count = 0;
 	unsigned long int converted_output = 0;
+	int count = 0;
 
+	/* checks for number polarity */
 	if (*str == '+')
+	{
 		str++;
+	}
 
+	/* Iterates through the string and convert each digit to integer */
 	for (count = 0;  str[count] != '\0'; count++)
 	{
-		if (str[count] >= '0' && str[count] <= '9')
+		if (str[count] <= '9' && str[count] >= '0')
 		{
 			converted_output *= 10;
 			converted_output += (str[count] - '0');
@@ -146,7 +158,10 @@ int exit_str_to_int(char *str)
 				return (-1);
 		}
 		else
+		{
 			return (-1);
+		}
+
 	}
 	return (converted_output);
 }
